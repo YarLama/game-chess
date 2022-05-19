05f0a49e7bac8fa7ab13cc3ffce9129e6fda909e
YarLama <gaideichuk1998@mail.ru>
2022-05-19 04:02:24 +0300

    add: start project

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './app.scss'
+
+const App: React.FC = () => {
+
+    return (
+        <div>
+            start project game-chess
+        </div>
+    );
+};
+
+export default App;
+  
+  
