2fec4752b41daf5f910abd16bb890569d9dfa6fb
YarLama <gaideichuk1998@mail.ru>
2022-05-21 15:33:13 +0300

    update: canMove basic method for each figure

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -11,7 +11,11 @@ export class King extends Figure {
         super(color, cell);
         this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
         this.name = FigureNames.KING;
-        
 	}
+
+    canMove(target: Cell): boolean {
+        if(!super.canMove(target)) return false;
+        return true;
+    }  
 
 }
