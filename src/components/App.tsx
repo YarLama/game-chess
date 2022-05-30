4ef6db5ecb98616c6e05513738c82fe4b8ab155d
YarLama <gaideichuk1998@mail.ru>
2022-05-30 22:47:05 +0300

    add: current player mechanic and update css button in tooltip

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,15 @@ import './app.scss'
 import React, { useEffect, useState } from 'react'
 import BoardComponent from './BoardComponent/BoardComponent';
 import { Board } from '../models/Board';
+import { Player } from '../models/Player';
+import { Colors } from '../models/Colors';
 
 
 const App: React.FC = () => {
     const [board, setBoard] = useState(new Board())
+    const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+    const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+    const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
     useEffect(() => {
         restart();
@@ -15,7 +20,12 @@ const App: React.FC = () => {
         const newBoard = new Board();
         newBoard.initCells();
         newBoard.addFigures();
+        setCurrentPlayer(whitePlayer);
         setBoard(newBoard);
+    }
+
+    function swapPlayer() {
+        setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
     }
 
     return (
@@ -24,10 +34,15 @@ const App: React.FC = () => {
                 <button onClick={restart}>Restart</button>
             </div>
             <div className="content">
-                <BoardComponent 
-                    board={board}
-                    setBoard={setBoard}
-                />
+                <div>
+                    <h3>{`Сейчас ходит: ${currentPlayer?.color.toUpperCase()}`}</h3>
+                    <BoardComponent 
+                        board={board}
+                        setBoard={setBoard}
+                        currentPlayer={currentPlayer}
+                        swapPlayer={swapPlayer}
+                    />
+                </div>
             </div>
         </div>
         
--- src/components/BoardComponent/BoardComponent.tsx
+++ src/components/BoardComponent/BoardComponent.tsx
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { Board } from '../../models/Board';
 import { Cell } from '../../models/Cell';
+import { Player } from '../../models/Player';
 import CellComponent from '../CellComponent/CellComponent';
 
 interface BoardProps {
     board: Board;
     setBoard: (board: Board) => void;
+    currentPlayer: Player;
+    swapPlayer: () => void;
 }
 
-const BoardComponent: React.FC<BoardProps> = ({board , setBoard}) => {
+const BoardComponent: React.FC<BoardProps> = ({board , setBoard, swapPlayer, currentPlayer}) => {
     const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
 
     function moveHandler(cell: Cell) {
         if(selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
             selectedCell.moveFigure(cell);
             setSelectedCell(null);
+            swapPlayer();
         }
     }
 
     function selectHandler(cell: Cell) {
-        if(cell.figure) setSelectedCell(cell);
+        if(cell.figure?.color === currentPlayer.color) setSelectedCell(cell);
     }
 
     function highLightCells() {
